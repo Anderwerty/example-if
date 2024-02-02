a93fa80c9ff4329daed73f367a7ffd4998e2059a
Andrii <andrii_shylin@gmail.com>
2024-02-02 20:26:37 +0200

    initial commit

---
+++ src/main/java/org/example/AnagramConsoleApplication.java
@@ -0,0 +1,8 @@
+//https://google.github.io/styleguide/javaguide.html#s5.2.1-package-names
+package org.example;
+
+public class AnagramConsoleApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
